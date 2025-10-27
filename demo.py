9478b4386567a9c74c66d7352610d9aea24a8b71
MonalishaPuhan <monalizapuhan.bdk@gmail.com>
2025-10-27 16:30:05 +0530

    added test

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
-a = 2
-b = 2
+"""A simple module that adds two numbers and prints the result."""
 
-print(a+b)
+A_VALUE = 2
+B_VALUE = 2
+
+
+def add_numbers():
+    """Return the sum of two numbers."""
+    return A_VALUE + B_VALUE
+
+
+if __name__ == "__main__":
+    print(add_numbers())
